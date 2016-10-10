82a2bb9d3a53cba44ac933cf27c962d8de086291
MitsuruFujiwara <fujiwara52jp@gmail.com>
2016-10-10 22:33:05 +0900

    add plot

--- keras_test.py
+++ keras_test.py
@@ -4,18 +4,36 @@ import numpy as np
 import pandas as pd
 
 from keras.models import Sequential
-model = Sequential()
-
 from keras.layers import Dense, Activation
-model.add(Dense(output_dim=2, input_dim=4))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=1))
-model.add(Activation("softmax"))
 
+import matplotlib.pyplot as plt
+
+# load dataset
+df = pd.read_csv('test_data.csv')
+
+# set header
+c = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9','X10', 'X11', 'X12',\
+'X13', 'X14', 'X15', 'X16', 'X17', 'X18', 'X19', 'X20', 'X21','X22', 'X23',\
+'X24', 'X25', 'X26', 'X27']
+trX = np.array(df[c].fillna(0))
+trY = np.array(df['Y'].fillna(0))
+
+# set moedel
+model = Sequential()
+model.add(Dense(output_dim=10, input_dim=trX.shape[1]))
+model.add(Activation('tanh'))
+model.add(Dense(output_dim=3, input_dim=10))
+model.add(Activation('relu'))
+model.add(Dense(output_dim=1, input_dim=3))
+model.add(Activation('softmax'))
+
+# training
 model.compile(loss='mean_squared_error', optimizer='sgd')
 
-df = pd.read_csv("test_data.csv")
-trX = df[["X1", "X2", "X3", "X4"]]
-trY = df["Y"]
-
-model.fit(trX, trY, nb_epoch=100)
+his  = model.fit(trX, trY, nb_epoch=10000)
+loss = his.history['loss']
+plt.plot(loss)
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
